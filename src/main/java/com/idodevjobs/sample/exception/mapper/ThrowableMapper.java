eb7bbc8498a72fd08bbe5beadbb0dc6d11116b70
idodevjobs <idodevjobs@gmail.com>
2014-11-08 08:42:09 -0600

    changes

--- src/main/java/com/idodevjobs/sample/exception/mapper/ThrowableMapper.java
+++ src/main/java/com/idodevjobs/sample/exception/mapper/ThrowableMapper.java
@@ -1,11 +1,1 @@
-package com.idodevjobs.sample.exception.mapper;
-
-import javax.ws.rs.core.Response;
-import javax.ws.rs.ext.ExceptionMapper;
-
-public class ThrowableMapper implements ExceptionMapper<Throwable> {
-    @Override
-    public Response toResponse(Throwable e) {
-        return Response.status(Response.Status.INTERNAL_SERVER_ERROR).header("Content-Type", "application/json").entity(new String("DATA ERROR")).build();
-    }
-}
+package com.idodevjobs.sample.exception.mapper;import javax.ws.rs.core.Response;import javax.ws.rs.ext.ExceptionMapper;public class ThrowableMapper implements ExceptionMapper<Throwable> {    @Override    public Response toResponse(Throwable e) {        return Response.status(Response.Status.INTERNAL_SERVER_ERROR).header("Content-Type", "application/json").entity(new String("DATA ERROR")).build();    }}
